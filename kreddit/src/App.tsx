570adc28906899755b5b22b76f208c8b1845ec6b
kiran-evans <thekiranevans@gmail.com>
2023-07-12 10:19:46 +0100

    Added basic structure and styling to header

--- kreddit/src/App.tsx
+++ kreddit/src/App.tsx
@@ -5,7 +5,13 @@ function App() {
     return (
         <>
             <header>
-                
+                <h1>kreddit</h1>
+
+                <input id="search" type="search" />
+
+                <button id="random">
+                    Random
+                </button>
             </header>
 
             <main>
