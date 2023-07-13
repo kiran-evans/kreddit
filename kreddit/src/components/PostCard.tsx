d3409e288395280ff9b2207336a8a7d3e6068ae7
kiran-evans <thekiranevans@gmail.com>
2023-07-13 11:35:29 +0100

    Adjusted styling and display comments and score count on post card

--- kreddit/src/components/PostCard.tsx
+++ kreddit/src/components/PostCard.tsx
@@ -3,7 +3,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 
-import { OpenInNew } from "@mui/icons-material";
+import { Comment, OpenInNew, Star } from "@mui/icons-material";
 
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 export const PostCard = (props: any) => {
@@ -13,22 +13,30 @@ export const PostCard = (props: any) => {
         thumbnail,
         thumbnail_width,
         author,
-        score,
         subreddit_name_prefixed,
         url,
         domain,
-        selftext
+        selftext,
+        num_comments,
+        score
     } = data;
     
     return (
         <article className="postCard">
-            {thumbnail_width && <img src={thumbnail} />}
+            {thumbnail_width && <div className="thumbnail">
+                <img src={thumbnail} />
+            </div>}
             <section>
-                {(!domain.includes("self") && !domain.includes("redd.it")) && <a className="originLink" href={url} target="_blank" rel="noopener noreferrer" title={`Go to original source on ${domain}`}><OpenInNew sx={{ fontSize: "small" }} />&nbsp;{domain}</a>}
+                {(!domain.includes("self") && !domain.includes("redd.it") && !domain.includes("reddit")) && <a className="originLink" href={url} target="_blank" rel="noopener noreferrer" title={`Go to original source on ${domain}`}><OpenInNew sx={{ fontSize: "1em" }} />&nbsp;{domain}</a>}
                 <h2>{title}</h2>
-                <p className="author"><a href={`https://www.reddit.com/u/${author}`} title={`Go to ${author}'s profile`} target="_blank" rel="noopener noreferrer">{author}</a></p>
-                <p className="subreddit"><a href={`https://www.reddit.com/${subreddit_name_prefixed}`} title={`Go to ${subreddit_name_prefixed} homepage`} target="_blank" rel="noopener noreferrer">{subreddit_name_prefixed}</a></p>
-                <p className="score">{Intl.NumberFormat('en-GB').format(score)}</p>
+                <div className="group">
+                    <p className="author"><a href={`https://www.reddit.com/u/${author}`} title={`Go to ${author}'s profile`} target="_blank" rel="noopener noreferrer">{author}</a></p>
+                    <p className="subreddit"><a href={`https://www.reddit.com/${subreddit_name_prefixed}`} title={`Go to ${subreddit_name_prefixed} homepage`} target="_blank" rel="noopener noreferrer">{subreddit_name_prefixed}</a></p>
+                </div>
+                <div className="group">
+                    <p className="comments" title={`${Intl.NumberFormat('en-GB').format(num_comments)} comments`}><Comment sx={{ fontSize: "1em" }} />&nbsp;{Intl.NumberFormat('en-GB').format(num_comments)}</p>
+                    <p className="score" title={`Score: ${Intl.NumberFormat('en-GB').format(score)}`}><Star sx={{ fontSize: "1em" }} />&nbsp;{Intl.NumberFormat('en-GB').format(score)}</p>
+                </div>
                 {selftext && <p className="selftext">{selftext}</p>}
             </section>
         </article>
