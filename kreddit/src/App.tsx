7bdce4c70c1de719fc45f7f89467e8c7e9ae32d4
kiran-evans <thekiranevans@gmail.com>
2023-07-14 12:10:54 +0100

    Search box now gets results and updates ui

--- kreddit/src/App.tsx
+++ kreddit/src/App.tsx
@@ -1,15 +1,14 @@
 import { Search, Shuffle } from '@mui/icons-material';
-import { useEffect } from 'react';
+import { FormEvent, useEffect } from 'react';
 import './App.scss';
 import { Dialog } from './components/Dialog';
 import { PostCard } from './components/PostCard';
 import { setDialog } from './lib/dialogSlice';
 import { useAppDispatch, useAppSelector } from './lib/hooks';
-import { setResults } from './lib/searchSlice';
+import { setQuery, setResults } from './lib/searchSlice';
 
 function App() {
     const search = useAppSelector((state) => state.search);
-    const dialog = useAppSelector((state) => state.dialog);
     const dispatch = useAppDispatch();
 
     // Get initial search from r/popular
@@ -27,7 +26,22 @@ function App() {
                 return;
             }
         })();
-    }, []);    
+    }, []);
+
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();        
+        try {
+            const res = await fetch(encodeURI(`https://www.reddit.com/search.json?q=${search.query}`), {
+                method: 'GET'
+            });
+            const { data } = await res.json();
+            dispatch(setResults(data.children));
+            
+        } catch (err: any) {
+            console.error(err);
+            return;
+        }
+    }
 
     return (
         <>
@@ -38,7 +52,9 @@ function App() {
                     <span>
                         <Search />
                     </span>
-                    <input id="search" type="search" />
+                    <form onSubmit={e => handleSubmit(e)}>
+                        <input id="search" type="search" onChange={e => dispatch(setQuery(e.target.value))} value={search.query} />
+                    </form>
                 </div>
 
                 <button id="random" onClick={() => dispatch(setDialog({ isOpen: true, data: []}))}>
