1c3382de0ddaa58fff6c65a056f5527ce4d0f8a1
kiran-evans <thekiranevans@gmail.com>
2023-07-20 12:10:22 +0100

    Added rendering of root element before each test

--- kreddit/src/__tests__/App.test.tsx
+++ kreddit/src/__tests__/App.test.tsx
@@ -1,10 +1,22 @@
 import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { Provider } from 'react-redux';
 import { describe, expect, test } from 'vitest';
 import App from '../App';
+import { store } from '../lib/store';
 
 describe("<App />", () => {
+    beforeEach(() => {
+        render(
+            <React.StrictMode>
+                <Provider store={store}>
+                    <App />
+                </Provider>
+            </React.StrictMode>
+        );
+    });
+
     test('Should render a <header>', () => {
-        render(<App />);
         expect(screen.getByText("kreddit")).toBeDefined();
     });
 });
